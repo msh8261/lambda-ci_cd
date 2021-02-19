3228f88b71e9bda8b9a6792b63b7b704e21f6532
mohser sh <sh1361m@gmail.com>
2021-02-19 14:16:00 +0800

    fixed

--- handler.py
+++ handler.py
@@ -4,7 +4,7 @@ import pyjokes
 
 def get_joke(event, context):
     body = {
-        "message": "--->>> Greetings from Github. Enjoy the jokes build here :)",
+        "message": "@@@ Greetings from Github. Enjoy the jokes build here :)",
         "joke":pyjokes.get_joke()
     }
     response = {
